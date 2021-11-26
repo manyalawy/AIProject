bbb293b4b260a5f5a5c9e9f28dc491c54a794a58
manyalawy <manyalawy@gmail.com>
2021-11-26 21:04:53 +0200

    half of statetogrid

--- src/Matrix.java
+++ src/Matrix.java
@@ -12,6 +12,9 @@ public class Matrix {
 
 
     public static void main(String[] args) {
+
+        //Notes:
+        //Add something to reformat state according to our state definetion
         Helpers.stateToGrid("5,5;2;4,3;2,1;2,0,0,4,0,3,0,1;3,1,3,2;4,4,3,3,3,3,4,4;4,0,17,1,2,54,0,0,46,4,1,22");
     }
 }
