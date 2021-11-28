54618f072c347da699702e308483f91cb0786cc5
ahmedhat1 <ahmed.sadekibrahim@gmail.com>
2021-11-28 16:10:09 +0200

    added dropped to hostage

--- src/Actions.java
+++ src/Actions.java
@@ -78,4 +78,5 @@ public class Actions {
 
         return newNode;
     }
+
 }
--- src/Matrix.java
+++ src/Matrix.java
@@ -22,8 +22,10 @@ public class Matrix {
         String state = Helpers.changeStateFormat("5,5;2;4,3;2,1;2,0,0,4,0,3,0,1;3,1,3,2;4,4,3,3,3,3,4,4;4,0,17,1,2,54,0,0,46,4,1,22");
         Grid grid = Helpers.stateToGrid(state);
         state = Helpers.gridToState(grid);
-        System.out.println(state);
-
+        // System.out.println(state);
+        Node node = new Node(null,Operators.LEFT,"5,5;2;4,3,0,0;2,1;2,0,0,4,0,3,0,1;3,3,3,3;4,4,3,3,3,3,4,4;4,0,17,false,1,2,54,false,0,0,46,false,4,1,22,false",0,0);
+        Actions.Right(node);
+        System.out.println(node.state);
         long end = System.currentTimeMillis();
         long elapsedTime = end - start;
         System.out.println(elapsedTime);
